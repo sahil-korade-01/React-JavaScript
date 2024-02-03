18e9c8b391d73fda92fab860597e94a95e919828
sahil-korade-01 <sahilkorade3@gmail.com>
2024-02-03 15:05:50 +0530

    B

--- Javascript/Basic/a5.js
+++ Javascript/Basic/a5.js
@@ -1,5 +1,5 @@
 // Write a JavaScript program to check whether a given integer is within 20 of 100 or 400.
-  // Define a function named testhundred that takes a parameter x
+ 
 function testhundred(x) {
 
     return ((Math.abs(100 - x) <= 20) || (Math.abs(400 - x) <= 20));
--- Javascript/Basic/a6.js
+++ Javascript/Basic/a6.js
@@ -1,25 +1,36 @@
 // Write a JavaScript program to check two given integers whether one is positive and another one is negative.  
 
 function positive_negative(x, y) {
-   
+
     if ((x < 0 && y > 0) || (x > 0 && y < 0)) {
-      
-      return true;
+
+        return true;
     } else {
-     
-      return false;
+
+        return false;
     }
-  }
-  
-  
-  console.log(positive_negative(2, 2));
-  
-  
-  console.log(positive_negative(-2, 2));
-  
-  
-  console.log(positive_negative(2, -2));
-  
-  
-  console.log(positive_negative(-2, -2)); 
-  
+}
+
+
+console.log(positive_negative(2, 2));
+
+
+console.log(positive_negative(-2, 2));
+
+
+console.log(positive_negative(2, -2));
+
+
+console.log(positive_negative(-2, -2));
+
+
+
+function p (x,y){
+    if ((x>0 && y<0) || (x<0 && y>0)){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(p(-4,5));
