8cd939bec4b4566f52b0cccbc9a4b6f05638354d
sahil-korade-01 <sahilkorade3@gmail.com>
2024-02-17 11:13:35 +0530

    missing Number

--- Javascript/DSA/1.js
+++ Javascript/DSA/1.js
@@ -1,29 +1,15 @@
 // Find the missing number in a given integer array of 1 to 100
 
 
-
-// function miss(array) {
-//   const n = 10;
-//   const tSum = (n * (n + 1)) / 2;
-
-//   const aSum = array.reduce((re, currentnum) => re + currentnum);
-
-//   return tSum - aSum;
-// }
-
-// const input = [1, 2, 3, 4, 5, 6, 8, 9, 10];
-// const missA = miss(input);
-
-// console.log(`The missing number is: ${missA}`);
-
-
-function mi (a){
-  const n = 5
-  const sum = (n*(n+1))/2
-  const asum = a.reduce((r,c)=>r+c)
-  return sum - asum;
+function miss (array ){
+    let n = 10
+    let sum = (n*(n+1))/2
+    let tSum = array.reduce((result , currentElement)=>result + currentElement)
+    return sum - tSum
 }
-const i = [1,3,4,5]
-const m = mi(i)
-console.log(m);
+
+
+ let Array = [1,2,3,4,5,6,8,9,10]
+ let missNumber = miss(Array)
+ console.log(`The missing number is : ${missNumber}`);
 
